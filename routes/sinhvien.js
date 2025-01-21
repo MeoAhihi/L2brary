4ccf01c6f5a6d3f244c42adaa9efd30b93d2bf1c
ViPhong <viphongly2804@gmail.com>
2025-01-21 14:14:09 +0700

    update sinhvien routes and views for CRUD operations; change home page title to 'L2brary'

--- routes/sinhvien.js
+++ routes/sinhvien.js
@@ -1,37 +1,40 @@
 const express = require("express");
+const SinhVien = require("../models/SinhVien");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const sinhviens = await SinhVien.find();
   res.render("sinhvien", {
-    sinhviens: [
-      {
-        fullName: "Nguyễn Văn A",
-        birthday: "01/01/1999",
-        gender: "Nam",
-        group: "1",
-        status: "Đang học",
-      },
-      {
-        fullName: "Nguyễn Văn B",
-        birthday: "02/02/1999",
-        gender: "Nữ",
-        group: "2",
-        status: "Đang học",
-      },
-      {
-        fullName: "Nguyễn Văn C",
-        birthday: "03/03/1999",
-        gender: "Nam",
-        group: "1",
-        status: "Đang học",
-      },
-    ],
+    sinhviens: sinhviens.map((sv) => ({
+      ...sv._doc,
+      birthday: sv.birthday.toISOString().split("T")[0],
+    })),
   });
 });
 
 router.get("/create", (req, res) => {
   res.render("TaoSinhVien");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const sinhVien = await SinhVien.findById(req.params.id);
+
+  res.render("editSinhVien", {
+    ...sinhVien._doc,
+    birthday: sinhVien.birthday.toISOString().split("T")[0],
+  });
+});
+
+router.post("/create", async (req, res) => {
+  const newSinhVien = new SinhVien(req.body);
+  await newSinhVien.save();
+  res.redirect("/sinhvien");
+});
+
+router.post("/delete/:id", async (req, res) => {
+  await SinhVien.findByIdAndDelete(req.params.id);
+  res.redirect("/sinhvien");
 });
 
 module.exports = router;
