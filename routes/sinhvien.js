7e7eff952db8b540ac7e95e1ddabda239812b873
ViPhong <viphongly2804@gmail.com>
2025-01-22 16:11:05 +0700

    add full name filtering and edit functionality for SinhVien

--- routes/sinhvien.js
+++ routes/sinhvien.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const SinhVien = require("../models/SinhVien");
+const { toVietnameseRegex } = require("../utils/vietnamese");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const sinhviens = await SinhVien.find();
+  const { fullName } = req.query;
+  const sinhviens = await SinhVien.find({
+    fullName: toVietnameseRegex(fullName),
+  });
+
   res.render("sinhvien", {
     sinhviens: sinhviens.map((sv) => ({
       ...sv._doc,
@@ -29,6 +34,19 @@ router.get("/edit/:id", async (req, res) => {
 router.post("/create", async (req, res) => {
   const newSinhVien = new SinhVien(req.body);
   await newSinhVien.save();
+  res.redirect("/sinhvien");
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, birthday, isMale, group, status } = req.body;
+  await SinhVien.findByIdAndUpdate(id, {
+    name,
+    birthday,
+    isMale,
+    group,
+    status,
+  });
   res.redirect("/sinhvien");
 });
 
